64a114206cbbf23568556ca0bf285e88df9e5e43
azhu000 <azhu000@citymail.cuny.edu>
2025-03-08 10:35:46 -0500

    changed nav bar properties

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -6,8 +6,8 @@ class navBarCons extends HTMLElement {
     this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://www.linkedin.com/in/anthony-zhu-cs/" style="display: inline-block; width: 40px; height: 50px; border: 0;"> 
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+            <a class="navbar-brand" href="https://www.linkedin.com/in/anthony-zhu-cs/" target="_blank"style="display: inline-block; width: 40px; height: 50px; border: 0;"> 
             <img src="assets/LinkedInIcon.png" alt="LinkedIn Icon" style="width: 100%; height: 100%;"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -15,10 +15,10 @@ class navBarCons extends HTMLElement {
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About Me</a>
+                    <a class="nav-link" href="aboutme.html">About Me</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact Me</a>
